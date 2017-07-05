4d8b99d89684b69cd15de62a0042070d79853f05
LukeGrafter-PC\Luke (Grafter) <grafteradd@gmail.com>
2017-07-05 20:03:35 +0200

    Update

---
+++ orbital.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="pl">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+    <title>$Orbital = 'Łukasz Szubart';</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="description" content="..." />
+    <meta name="keywords" content="...." />
+    <link rel="stylesheet" type="text/css" href="css/orbital.css" />
+        
+</head>
+	
+<body>
+	<div id="0">
+		<div class="sphare_base">base</div>
+		<div class="sphare_test">test 1</div>
+		<div class="sphare_test1">:test 2:</div>
+		<div class="sphare_test2">.:test 3:.</div>
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	</div>
+</body>
+	
+</html>
